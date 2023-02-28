8ca614b706e52e39714bdc19fbf849777efa179b
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-02-27 21:38:43 -0800

    saveral changes done to CSS file.

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 "use strict";
 
-const menuBtn = document.getElementById('btn_menu');
-
-
+const menuBtn = document.getElementById('menu_btn');
 
 
 
 menuBtn.addEventListener('click', (e) => {
 
-    var menuItems = document.getElementById('xxx');
+    menuBtn.classList.toggle("change");
+
+    var menuItems = document.getElementById('menu_list');
     var conteinerOne = document.querySelector('.conteiner_1');
 
     if ( menuItems.style.display === 'none' ) {
         menuItems.style.display = 'block'
         conteinerOne.style.display = 'none'
+        
     } else {
         menuItems.style.display = 'none';
         conteinerOne.style.display = 'flex'
